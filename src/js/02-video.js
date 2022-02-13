0f8059c71dc54e31e13a04a888b3856b0440582a
goit-markup-hw-01 <547736@gmail.com>
2022-02-14 01:00:57 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', onStart);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+
+  if (data.seconds === data.duration) {
+    player.off('timeupdate');
+    localStorage.removeItem('videoplayer-current-time');
+  }
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+const time = localStorage.getItem('videoplayer-current-time');
+const currentTime = time > null ? time : 0;
+player.setCurrentTime(currentTime);
+
+function onStart() {
+  player.on('timeupdate', throttle(onPlay, 1000));
+}
